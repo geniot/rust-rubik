a70b288191a28339a2a0d725c77d36e3add3ef1c
Vitaly Sazanovich <vitaly.sazanovich@gmail.com>
2026-05-23 09:55:29 +0200

    Add vsync, use arch to load lib

--- build.rs
+++ build.rs
@@ -1,9 +1,18 @@
+#![feature(str_as_str)]
+
 use std::env;
 use std::path::Path;
 
 fn main() {
     println!("cargo:rustc-link-arg=-lX11");
-    println!("cargo:rustc-link-lib=static=raylib");
+    println!(
+        "{}{}",
+        "cargo:rustc-link-lib=static=raylib_",
+        String::from(env::consts::ARCH)
+    );
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!("cargo:rustc-link-search=native={}", Path::new(&dir).join("lib").display());
+    println!(
+        "cargo:rustc-link-search=native={}",
+        Path::new(&dir).join("lib").display()
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use std::ffi::CString;
-use crate::bindings::{
-    BeginDrawing, Color, DrawText, EndDrawing, InitWindow, WindowShouldClose,
-};
+use crate::bindings::{BeginDrawing, Color, ConfigFlags_FLAG_VSYNC_HINT, DrawText, EndDrawing, InitWindow, SetConfigFlags, WindowShouldClose};
 use std::os::raw::c_char;
 #[allow(warnings)]
 mod bindings;
 
 fn main() {
+    println!("{}", std::env::consts::ARCH);
     let c_string = CString::new("Hello!").expect("CString::new failed");
     let name: *mut c_char = c_string.into_raw();
     let color = Color {
@@ -17,6 +16,7 @@ fn main() {
     };
 
     unsafe {
+        SetConfigFlags(ConfigFlags_FLAG_VSYNC_HINT);
         InitWindow(800, 600, name);
         while !WindowShouldClose() {
             BeginDrawing();
